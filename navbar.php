cd1843a22ac60a858045b7eec143e27925bcb3a6
fahad-gcet <fms525@outlook.com>
2017-08-01 16:36:48 +0530

    fixed navbar

--- navbar.php
+++ navbar.php
@@ -1,3 +1,9 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+
 <?php include('server.php') ?>
 <nav class="navbar navbar-inverse">
   <div class="container-fluid">
@@ -14,10 +20,12 @@
         <li><a class="navbar-brand" href=".">Profile Hub</a></li>
       </ul>
     </div>
+
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
     <?php
     if (isset($_SESSION['username'])) {
       ?>
-      <ul class="nav navbar-nav navbar-right">
+      <ul class="nav navbar-nav navbar-right " >
       <li><a href="profile.php"><button type="button" class="btn btn-primary btn-block ">Edit Profile</button></a></li>
       <li><a href="index.php?logout=1"><button type="button" class="btn btn-danger btn-block " >Log Out</button></a></li>
       
@@ -25,7 +33,7 @@
       <?php
     } else {
     ?>
-    <ul class="nav navbar-nav navbar-right">
+    <ul class="nav navbar-nav navbar-right ">
       <li><a href="login.php"><button type="button" class="btn btn-success btn-block ">Log In</button></a></li>
      <li><a href="register.php"><button type="button" class="btn btn-primary btn-block ">Sign Up</button></a></li>
       </ul>
@@ -33,5 +41,7 @@
 
 
   
-  </div><!-- /.container-fluid -->
+  </div>
+  </div>
 </nav>
+
